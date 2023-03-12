f54ddef4d17f8f08df600d71bf125c0f2f472c1c
alfonsogarciadecorral <alfonso.garcia.decorral@gmail.com>
2023-03-12 10:31:46 +0000

    simple sarsa implemented for taxi-v3 task

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,9 +1,9 @@
 import numpy as np
 from collections import defaultdict
-
+import random
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, alpha=0.1, epsilon=0.1, gamma=0.9):
         """ Initialize agent.
 
         Params
@@ -11,20 +11,25 @@ class Agent:
         - nA: number of actions available to the agent
         """
         self.nA = nA
-        self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.Q = defaultdict(lambda: np.zeros(self.nA))   
+        self.eps = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
 
     def select_action(self, state):
-        """ Given the state, select an action.
-
+        """Selects epsilon-greedy action for supplied state.
+        
         Params
         ======
-        - state: the current state of the environment
-
-        Returns
-        =======
-        - action: an integer, compatible with the task's action space
+            Q (dictionary): action-value function
+            state (int): current state
+            nA (int): number actions in the environment
+            eps (float): epsilon
         """
-        return np.random.choice(self.nA)
+        if random.random() > self.eps: # select greedy action with probability epsilon
+            return np.argmax(self.Q[state])
+        else:                     # otherwise, select an action randomly
+            return random.choice(np.arange(self.nA))
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +42,7 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        greedy_action = self.select_action(next_state)
+        self.Q[state][action] += self.alpha*(reward +
+                                             self.gamma*self.Q[next_state][greedy_action] -
+                                             self.Q[state][action])
